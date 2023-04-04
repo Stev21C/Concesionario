446c0cc2c50123474709ff35f7366151d105efda
Steven Correa <s21c99@gmail.com>
2023-04-03 22:12:55 -0500

    Inicio de proyecto

---
+++ src/test/java/com/projectcoches/ProjectConcesionarioCoches/ProjectConcesionarioCochesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectcoches.ProjectConcesionarioCoches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectConcesionarioCochesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
